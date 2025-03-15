9b777bd08bcf2d83ac8a4ae57eea9e36d4dd764d
Nuno Costa <nflcosta@gmail.com>
2025-03-15 18:44:29 +0000

    .

--- src/content/pages/pt/testimonials.md
+++ src/content/pages/pt/testimonials.md
@@ -9,8 +9,10 @@ locale_alternates:
   - language_code: en
     link: http://google.com
 seo:
-  page_title: Testemunhos da Top Bike Tours Portugal: Experiências de Clientes
-  page_description: Leia testemunhos sobre as experiências de passeios de bicicleta e pedestres com a Top Bike Tours Portugal, fundada em 2013 em Porto.
+  page_title: 'Testemunhos da Top Bike Tours Portugal: Experiências de Clientes'
+  page_description:
+    Leia testemunhos sobre as experiências de passeios de bicicleta e pedestres
+    com a Top Bike Tours Portugal, fundada em 2013 em Porto.
   canonical_url:
   featured_image: 'src/assets/images/bike-tours-in-portugal.jpg'
   featured_image_alt: Sobre nós
@@ -25,12 +27,24 @@ content_blocks:
       color: '#333333'
     text:
       markdown_content: >-
-        Fundada em 2013, a Top Bike Tours Portugal, Unipessoal, Lda, aqui referida como "Top Walking Tours Portugal", é uma empresa com experiência em passeios pedestres e de bicicleta na cidade do Porto e em rotas de ciclismo de longa distância pelo norte da Península Ibérica e por todo Portugal. As nossas atividades são coordenadas por profissionais de turismo, com vasto conhecimento em património e desporto.
+        Fundada em 2013, a Top Bike Tours Portugal, Unipessoal, Lda, aqui
+        referida como "Top Walking Tours Portugal", é uma empresa com
+        experiência em passeios pedestres e de bicicleta na cidade do Porto e em
+        rotas de ciclismo de longa distância pelo norte da Península Ibérica e
+        por todo Portugal. As nossas atividades são coordenadas por
+        profissionais de turismo, com vasto conhecimento em património e
+        desporto.
 
 
-        No final de 2017, mudámos o nosso nome anterior "Fold n' Visit" para "Top Bike Tours Portugal".
+        No final de 2017, mudámos o nosso nome anterior "Fold n' Visit" para
+        "Top Bike Tours Portugal".
 
-        Desde 2013, oferecemos uma vasta gama de diferentes passeios pela cidade do Porto. Hoje em dia, o nosso negócio principal é a criação de pacotes de férias de ciclismo e caminhadas. A oferta de passeios pela cidade foi reduzida, assim como o negócio de aluguer de bicicletas para distâncias maiores. É possível acompanhar a nossa história nas avaliações do Tripadvisor.
+        Desde 2013, oferecemos uma vasta gama de diferentes passeios pela cidade
+        do Porto. Hoje em dia, o nosso negócio principal é a criação de pacotes
+        de férias de ciclismo e caminhadas. A oferta de passeios pela cidade foi
+        reduzida, assim como o negócio de aluguer de bicicletas para distâncias
+        maiores. É possível acompanhar a nossa história nas avaliações do
+        Tripadvisor.
       color: '#393939'
     image:
       image_path: /src/assets/images/bike-tours-in-portugal.jpg
--- src/layouts/common/CookieConsentConfig.ts
+++ src/layouts/common/CookieConsentConfig.ts
@@ -1,5 +1,17 @@
 import type { CookieConsentConfig } from 'vanilla-cookieconsent';
-import CookieDataConsent from 'src/../data/cookie.json';
+
+export const CAT_NECESSARY = 'necessary';
+export const CAT_ANALYTICS = 'analytics';
+export const CAT_ADVERTISEMENT = 'advertisement';
+export const CAT_FUNCTIONALITY = 'functionality';
+export const CAT_SECURITY = 'security';
+export const SERVICE_AD_STORAGE = 'ad_storage';
+export const SERVICE_AD_USER_DATA = 'ad_user_data';
+export const SERVICE_AD_PERSONALIZATION = 'ad_personalization';
+export const SERVICE_ANALYTICS_STORAGE = 'analytics_storage';
+export const SERVICE_FUNCTIONALITY_STORAGE = 'functionality_storage';
+export const SERVICE_PERSONALIZATION_STORAGE = 'personalization_storage';
+export const SERVICE_SECURITY_STORAGE = 'security_storage';
 
 export const config: CookieConsentConfig = {
   revision: 1,
@@ -42,6 +54,91 @@ export const config: CookieConsentConfig = {
   language: {
     default: 'en',
     autoDetect: 'browser',
-    translations: CookieDataConsent
+    translations: {
+      en: {
+        // See: https://support.google.com/tagmanager/answer/10718549?hl=en
+        consentModal: {
+          title: 'We use cookies',
+          description:
+            'This website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent.',
+          acceptAllBtn: 'Accept all',
+          acceptNecessaryBtn: 'Reject all',
+          showPreferencesBtn: 'Manage Individual preferences'
+        },
+        preferencesModal: {
+          title: 'Manage cookie preferences',
+          acceptAllBtn: 'Accept all',
+          acceptNecessaryBtn: 'Reject all',
+          savePreferencesBtn: 'Accept current selection',
+          closeIconLabel: 'Close modal',
+          sections: [
+            {
+              title: 'Cookie usage',
+              description:
+                'We use cookies to ensure the basic functionalities of the website and to enhance your online experience.'
+            },
+            {
+              title: 'Strictly necessary cookies',
+              description:
+                'These cookies are essential for the proper functioning of the website, for example for user authentication.',
+              linkedCategory: CAT_NECESSARY
+            },
+            {
+              title: 'Analytics',
+              description:
+                'Cookies used for analytics help collect data that allows services to understand how users interact with a particular service. These insights allow services both to improve content and to build better features that improve the user’s experience.',
+              linkedCategory: CAT_ANALYTICS,
+              cookieTable: {
+                headers: {
+                  name: 'Name',
+                  domain: 'Service',
+                  description: 'Description',
+                  expiration: 'Expiration'
+                },
+                body: [
+                  {
+                    name: '_ga',
+                    domain: 'Google Analytics',
+                    description:
+                      'Cookie set by <a href="https://business.safety.google/adscookies/">Google Analytics</a>',
+                    expiration: 'Expires after 12 days'
+                  },
+                  {
+                    name: '_gid',
+                    domain: 'Google Analytics',
+                    description:
+                      'Cookie set by <a href="https://business.safety.google/adscookies/">Google Analytics</a>',
+                    expiration: 'Session'
+                  }
+                ]
+              }
+            },
+            {
+              title: 'Advertising',
+              description:
+                'Google uses cookies for advertising, including serving and rendering ads, personalizing ads (depending on your ad settings at <a href=\"https://g.co/adsettings\">g.co/adsettings</a>), limiting the number of times an ad is shown to a user, muting ads you have chosen to stop seeing, and measuring the effectiveness of ads.',
+              linkedCategory: CAT_ADVERTISEMENT
+            },
+            {
+              title: 'Functionality',
+              description:
+                'Cookies used for functionality allow users to interact with a service or site to access features that are fundamental to that service. Things considered fundamental to the service include preferences like the user’s choice of language, product optimizations that help maintain and improve a service, and maintaining information relating to a user’s session, such as the content of a shopping cart.',
+              linkedCategory: CAT_FUNCTIONALITY
+            },
+            {
+              title: 'Security',
+              description:
+                'Cookies used for security authenticate users, prevent fraud, and protect users as they interact with a service.',
+              linkedCategory: CAT_SECURITY
+            },
+            {
+              title: 'More information',
+              description:
+                'For any queries in relation to the policy on cookies and your choices, please <a href="https://www.example.com/contacts">contact us</a>.'
+            }
+          ]
+        }
+      }
+    }
   }
 };
